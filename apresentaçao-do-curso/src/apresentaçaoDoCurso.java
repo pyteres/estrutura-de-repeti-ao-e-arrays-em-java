06123b6631a08a83ff3b285375059dcab5f61938
pyteres <ianpimenta18@gmail.com>
2023-02-06 05:13:51 -0300

    prologo-curso

---
+++ apresentaçao-do-curso/src/apresentaçaoDoCurso.java
@@ -0,0 +1,14 @@
+public class apresentaçaoDoCurso {
+    /*
+     * neste curso iremos aprender sobre:
+     * comando while:
+     * comando do-while:
+     * comando for:
+     * Controle de repetiçao break e continue:
+     * Arrays:
+     * Comando for eatch:
+     */
+    public static void main(String[] args) {
+        System.out.println("E isso que iremos aprender nesta nova fase desta jornada de aprendizado e sejam todos muito bem vindos");
+    }
+}
